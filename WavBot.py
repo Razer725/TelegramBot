efe11048c7dcf0de67478cd0126179d2386ff547
Ivan Krasilnikov <krasivan93@gmail.com>
2020-02-08 14:54:35 +0300

    'sound_libraries'

--- WavBot.py
+++ WavBot.py
@@ -1,6 +1,19 @@
 import telebot
 import os
 import sqlite3
+import wave
+from urllib.request import urlopen
+import soundfile
+from pydub import AudioSegment
+import io
+
+# data = soundfile.read('t_voice5328001140795639231.ogg')
+# soundfile.write('tawda.wav', data, 16000)
+
+
+sound = AudioSegment.from_ogg(r'C:\Users\Иван\PycharmProjects\TeleBotWavSave\DescenteInfinie.ogg')
+
+
 
 if os.path.exists('TeleBotWavSave.db'):
     conn = sqlite3.connect('TeleBotWavSave.db')
@@ -16,24 +29,28 @@ with conn:
     cur.execute("SELECT * FROM BOT_SAVE")
     rows = cur.fetchall()
     print(rows)
-#
-# bot = telebot.TeleBot('1008610085:AAEfDVp8c_xR2-EZ4f5mELCejbMXFS5i5Tc')
-#
-#
-# @bot.message_handler(commands=['start'])
-# def start_message(message):
-#     bot.send_message(message.chat.id, 'Привет')
-#
-#
+
+bot = telebot.TeleBot('1008610085:AAEfDVp8c_xR2-EZ4f5mELCejbMXFS5i5Tc')
+
+
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    bot.send_message(message.chat.id, 'Привет')
+
+
 # @bot.message_handler(content_types=['voice'])
 # def save_voice(message):
 #     bot.send_message(message.chat.id, 'Сохраняю')
 #     file_id = message.voice.file_id
 #     file = bot.get_file(file_id)
+#
+#
 #     downloaded_file = bot.download_file(file.file_path)
 #     user_id = message.from_user.id
-#     with open(os.getcwd() + '/' + str(user_id) + '.ogg', 'wb') as file_out:
+#
+#     with open(os.getcwd() + '\\' + str(file_id) + '.ogg', 'wb') as file_out:
 #         file_out.write(downloaded_file)
-
-
+#
+#
 # bot.polling()
+
