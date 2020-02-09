de45bd2ba343df515b45e410d6587360423a26ce
Ivan Krasilnikov <krasivan93@gmail.com>
2020-02-09 19:58:37 +0300

    Final version

--- WavBot.py
+++ WavBot.py
@@ -2,9 +2,11 @@ import telebot
 import os
 import sqlite3
 import subprocess
-from telebot import apihelper
-import face_recognition
 import cv2
+
+"""The bot automatically saves all audio messages from groups to the database.
+Converts all audio messages to wav format with a sampling frequency of 16kHz. Files are placed in a folder: Voice
+Detects the presence of a face in the photos being sent, saves only where it is"""
 
 TELEGRAM_API_TOKEN = '1008610085:AAEfDVp8c_xR2-EZ4f5mELCejbMXFS5i5Tc'
 bot = telebot.TeleBot(TELEGRAM_API_TOKEN)
@@ -18,7 +20,7 @@ def convert_to_wav(path, file_directory, fileid, user_id):
     conn = sqlite3.connect('TeleBotWavSave.db')  # connect to db
     cursor = conn.cursor()
 
-    # Запрос в командную строку для обращения к ffmpeg
+    # Request to the command line for access to ffmpeg
     converted_path_to_file = '{}audio_message_{}'.format(file_directory, fileid) + '.wav'
     converted_file_id = 'audio_message_{}'.format(fileid) + '.wav'
     command = [r'Voice\ffmpeg.exe', '-i', path + '.ogg', '-ar', '16000', converted_path_to_file]
@@ -28,21 +30,22 @@ def convert_to_wav(path, file_directory, fileid, user_id):
     cursor.execute("INSERT INTO BOT_SAVED_FILES (FILE_ID, USER_ID, FILE_TYPE, PATH_TO_FILE) VALUES (?,?,?,?)",
                    (converted_file_id, user_id, 'wav', converted_path_to_file))
     conn.commit()
+    conn.close()
     return
 
 
 if os.path.exists('TeleBotWavSave.db'):
-    conn = sqlite3.connect('TeleBotWavSave.db')
+    pass
 else:
     conn = sqlite3.connect('TeleBotWavSave.db')
     cursor = conn.cursor()
     cursor.execute("""CREATE TABLE BOT_SAVED_FILES
                     (FILE_ID, USER_ID, FILE_TYPE, PATH_TO_FILE)
                     """)
+    conn.commit()
+    conn.close()
 
-conn = sqlite3.connect('TeleBotWavSave.db')  # connect to db
-cursor = conn.cursor()
-
+conn = sqlite3.connect('TeleBotWavSave.db')
 with conn:
     cur = conn.cursor()
     cur.execute("SELECT * FROM BOT_SAVED_FILES")
@@ -52,7 +55,9 @@ with conn:
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
-    bot.send_message(message.chat.id, 'Привет')
+    bot.send_message(message.chat.id, """The bot automatically saves all audio messages from groups to the database.
+Converts all audio messages to wav format with a sampling frequency of 16kHz. Files are placed in a folder: Voice
+Detects the presence of a face in the photos being sent, saves only where it is""")
 
 
 @bot.message_handler(content_types=['voice'])
@@ -77,12 +82,16 @@ def voice_processing(message):
         cursor.execute("INSERT INTO BOT_SAVED_FILES (FILE_ID, USER_ID, FILE_TYPE, PATH_TO_FILE) VALUES (?,?,?,?)",
                        (file_id, user_id, 'ogg', path_to_file))
         conn.commit()
+        conn.close()
 
         convert_to_wav(path, file_directory, str(file_id), user_id)
 
 
 @bot.message_handler(content_types=['photo'])
 def photo_processing(message):
+    """
+    Bot saves only photos with faces
+    """
     file_id = message.photo[len(message.photo) - 1].file_id
     file_info = bot.get_file(file_id)
     downloaded_file = bot.download_file(file_info.file_path)
@@ -99,42 +108,29 @@ def photo_processing(message):
     # Load the cascade
     face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
     # Read the input image
-    img = cv2.imread('Photo\\test.jpg')
+    img = cv2.imread('Photo\\' + str(file_id) + '.jpg')
     # Convert into grayscale
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     # Detect faces
     faces = face_cascade.detectMultiScale(gray, 1.1, 4)
-
-    if len(faces) > 0:
-        print('face found')
+    if len(faces) > 0:  # If face found
+        # Connect to db
+        conn = sqlite3.connect('TeleBotWavSave.db')  # connect to db
+        cursor = conn.cursor()
+        # Insert in database
+        cursor.execute("INSERT INTO BOT_SAVED_FILES (FILE_ID, USER_ID, FILE_TYPE, PATH_TO_FILE) VALUES (?,?,?,?)",
+                       (file_id, user_id, 'jpg', path_to_file))
+        conn.commit()
+        conn.close()
     else:
-        print('No face')
+        os.remove(path_to_file)
 
-    # Draw rectangle around the faces
-    for (x, y, w, h) in faces:
-        cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
-    # Display the output
-    cv2.imshow('img', img)
-    cv2.waitKey()
+    # # Draw rectangle around the faces
+    # for (x, y, w, h) in faces:
+    #     cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+    # # Display the output
+    # cv2.imshow('img', img)
+    # cv2.waitKey()
 
-
-# Load the cascade
-face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-# Read the input image
-img = cv2.imread('test2.jpg')
-# Convert into grayscale
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-# Detect faces
-faces = face_cascade.detectMultiScale(gray, 1.1, 4)
-if len(faces) > 0:
-    print('face found')
-else:
-    print('No face')
-# Draw rectangle around the faces
-for (x, y, w, h) in faces:
-    cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
-# Display the output
-cv2.imshow('img', img)
-cv2.waitKey()
 
 bot.polling()
